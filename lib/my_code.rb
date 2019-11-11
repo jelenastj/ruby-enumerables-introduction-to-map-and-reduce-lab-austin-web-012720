7c8783ba5fc0a78c1c02250fb46780cfc3a4bef0
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-11 22:12:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,7 @@ def reduce_to_total(source_array, starting_point)
   i=0 
   total = 0 
   while i<source_array.lenght do
-    if !starting_point
+    if starting_point = 0 
       total +=source_array[i]
       i+=1
     end
