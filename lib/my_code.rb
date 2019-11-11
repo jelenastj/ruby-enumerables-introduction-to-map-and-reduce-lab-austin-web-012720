b43ec874e083fe5b9e800ae5f75bb995aa0d5a25
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-11 22:05:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,7 +36,7 @@ def map_to_negativize(source_array)
     new_array=[]
     i=0 
     while i<source_array.length do
-      new_array.push(source_array[i]*2)
+      new_array.push(source_array[i]**2)
       i+=1 
     end
     new_array
