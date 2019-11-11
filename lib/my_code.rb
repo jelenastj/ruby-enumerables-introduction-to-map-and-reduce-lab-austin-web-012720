48d9d5619c871704d09a293852b28fd26c2126e2
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-11 22:32:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,17 +54,17 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 
-# def reduce_to_all_true(source_array)
-#   i=0 
-#   while i<source_array.length do
-#     if source_array[i] = true 
-#       i+=1 
-#     end
-#     true
-#   else
-#     false
+def reduce_to_all_true(source_array)
+  i=0 
+  while i<source_array.length do
+    if source_array[i] = true 
+      i+=1 
+    end
+    true
+  else
+    false
     
-# end
+end
   
   
   
