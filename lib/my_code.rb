aa72e2bb049712c46226419da6935783891ea226
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-11 22:33:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,8 +58,9 @@ def reduce_to_all_true(source_array)
   i=0 
   
   while i<source_array.length do
-    if source_array[i] = true 
+    if source_array[i] 
       i+=1 
+      return true
     end
     true
   else
